b582c5815c398043766ca68526119b8ec6b0451c
mahmoudb192000 <mahmoudelboushi0909@gmail.com>
2022-06-14 23:43:02 +0200

    u

---
+++ js/connect us.js
@@ -0,0 +1,45 @@
+/*setTimeout(wait, 2000)
+
+function wait() {
+   document.getElementById("wait").remove();
+}*/
+
+
+function myFunction() {
+   if (document.documentElement.scrollTop > 0) {
+      document.getElementById("up1").style.display = "block";
+   } else {
+      document.getElementById("up1").style.display = "none";
+   }
+}
+
+function up1() {
+   document.documentElement.scrollTop = 0;
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+ }
+ 
+ function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+ }
+ function icuf1(){
+   window.open("https://www.facebook.com/profile.php?id=100012510252975");
+ }
+ function icuf2(){
+   window.open("https://www.facebook.com/profile.php?id=100007271748615");
+ }
+ function icuf3(){
+   window.open("https://www.facebook.com/Mohamed.Ayman72");
+ }
+ function icuf4(){
+   window.open("https://www.facebook.com/profile.php?id=100009303497667");
+ }
+ function icuf5(){
+   window.open("https://www.facebook.com/profile.php?id=100009303497667");
+ }
+ function icuf6(){
+   window.open("https://www.facebook.com/mohamed.ayman.779642");
+ }
+ 
+ 
