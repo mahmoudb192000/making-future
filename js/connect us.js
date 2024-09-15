4c76f061aeea2c90ecaeb3180a9a38407edc670c
mahmoud elboushy <101577719+mahmoudb192000@users.noreply.github.com>
2024-09-15 22:34:23 +0300

    Update connect us.js

--- js/connect us.js
+++ js/connect us.js
@@ -23,32 +23,6 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
  }
  function icuf1(){
-   window.open("https://www.facebook.com/profile.php?id=100012510252975");
+   window.open("https://mahmoudb192000.github.io/my-website");
  }
- function icuf2(){
-   window.open("https://www.facebook.com/profile.php?id=100007271748615");
- }
- function icuf3(){
-   window.open("https://www.facebook.com/Mohamed.Ayman72");
- }
- function icuf4(){
-   window.open("https://www.facebook.com/Abdo.Uwk07");
- }
- function icuf5(){
-   window.open("https://www.facebook.com/profile.php?id=100009303497667");
- }
- function icuf6(){
-   window.open("https://www.facebook.com/mohamed.ayman.779642");
- }
- function icuf7(){
-   window.open("https://www.facebook.com/profile.php?id=100018411032955");
- }
- function icuf8(){
-   window.open("https://www.facebook.com/profile.php?id=100007579565246");
- }
- function icuf9(){
-   window.open("https://www.facebook.com/profile.php?id=100008104689914");
- }
- function icuf10(){
-   window.open("https://www.facebook.com/elsyed.eldamshety");
- }
+ 
